d500505359013b916eba0f45fbcbfeb1de42c290
GenyaSochi <eunasedkina@gmail.com>
2025-07-28 22:39:28 +0300

    _

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <HeaderComponent></HeaderComponent>
+  <NuxtPage />
+  <FooterComponent></FooterComponent>
+  <UpButtonComponent></UpButtonComponent>  
+</template>
+<style>
+@import url(~/assets/reset.css);
+@import url(~/assets/style.css);
+</style>
