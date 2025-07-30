1809751a6d4ac99a3586c4d1a592b82859ef962b
GenyaSochi <eunasedkina@gmail.com>
2025-07-30 20:47:09 +0300

    _

--- app/components/HeaderComponent.vue
+++ app/components/HeaderComponent.vue
@@ -1,8 +1,22 @@
 <template>
- <h1>AAA</h1>
+  <header>
+    <div class="allheader">
+      <p>главная</p>
+      <p>услуги</p>
+      <p>о нас</p>
+      <p>отзывы</p>
+      <p>контакты</p>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped></style>
+<style scoped>
+.allheader {
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+}
+</style>
