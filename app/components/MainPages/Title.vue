3a67b29be8888137067f19ee0454d4015d16d904
GenyaSochi <eunasedkina@gmail.com>
2025-08-06 22:27:17 +0300

    _

--- app/components/MainPages/Title.vue
+++ app/components/MainPages/Title.vue
@@ -3,14 +3,26 @@
   <div class="main">
     <h1>юридическая помощь по семейным и административным делам</h1>
     <div>
-      <p>Отмена судебных приказов за 48 часов</p>
+      <p class="text">Отмена судебных приказов за 48 часов</p>
     </div>
-    <h2>Юристы с 20-летним опытом в узкопрофильных направлениях права найдут оптимальное решение для
+    <h2 class="text">Юристы с 20-летним опытом в узкопрофильных направлениях права найдут оптимальное решение для
       вашего случая</h2>
   </div>  
-  </div>
+</div>
+,<figure>
+  <img src="/img/photo1.webp" alt="photo" class="photo">
+  <figcaption>NNNNNNNNNNNNNNNNN</figcaption>
+</figure>
 </template>
 <script setup lang="ts"></script>
 <style scoped>
+.text {
+  text-align: center;
+  padding-top: 20px;
+  font-size: 24px;
+}
+.photo {
+  height: 200px;
+}
 
 </style>
